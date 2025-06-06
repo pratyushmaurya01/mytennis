bbf93588805858804aed73324f2a0ea90f929cdb
pratyushmaurya01 <pratyushmaurya0@gmail.com>
2025-06-06 13:42:36 +0530

    Removed unwanted migration files

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", default=False, cast=bool)
+
 ALLOWED_HOSTS = ['*']
 
 
